ad004b71532f7262d3b323c9c92bd1f09258b43e
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-12-04 20:04:43 +0900

    Initial Commit

---
+++ USPMapNearMe/USPMapNearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPMapNearMeApp.swift
+//  USPMapNearMe
+//
+//  Created by joe on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct USPMapNearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
