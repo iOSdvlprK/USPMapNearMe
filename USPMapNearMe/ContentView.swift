f4f43794d316c928546557a1bbad9d27d984ddd8
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2025-12-08 23:27:04 +0900

    add user location annotation
    - added NSLocationWhenInUseUsageDescription to the project settings to request user location permission
    - updated ContentView to display the user's location on the map using UserAnnotation and initialized the shared LocationManager

--- USPMapNearMe/ContentView.swift
+++ USPMapNearMe/ContentView.swift
@@ -11,26 +11,29 @@ import MapKit
 struct ContentView: View {
     @State private var query: String = ""
     @State private var selectedDetent: PresentationDetent = .fraction(0.15)
+    @State private var locationManager = LocationManager.shared
     
     var body: some View {
         ZStack {
-            Map()
-                .sheet(isPresented: .constant(true)) {
-                    VStack {
-                        TextField("Search", text: $query)
-                            .textFieldStyle(.roundedBorder)
-                            .padding()
-                            .onSubmit {
-                                // code fired when you click return in TextField
-                            }
-                        
-                        Spacer()
-                    }
-                    .presentationDetents([.fraction(0.15), .medium, .large], selection: $selectedDetent)
-                    .presentationDragIndicator(.visible)
-                    .interactiveDismissDisabled()
-                    .presentationBackgroundInteraction(.enabled(upThrough: .medium))
+            Map {
+                UserAnnotation()
+            }
+            .sheet(isPresented: .constant(true)) {
+                VStack {
+                    TextField("Search", text: $query)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                        .onSubmit {
+                            // code fired when you click return in TextField
+                        }
+                    
+                    Spacer()
                 }
+                .presentationDetents([.fraction(0.15), .medium, .large], selection: $selectedDetent)
+                .presentationDragIndicator(.visible)
+                .interactiveDismissDisabled()
+                .presentationBackgroundInteraction(.enabled(upThrough: .medium))
+            }
         }
     }
 }
